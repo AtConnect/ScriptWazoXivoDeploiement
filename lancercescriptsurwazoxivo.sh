b182522fa3b2148320c45300059028408d43d5f0
AtConnect <49723963+AtConnect@users.noreply.github.com>
2019-04-18 10:01:56 +0200

    Update lancercescriptsurwazoxivo.sh

--- lancercescriptsurwazoxivo.sh
+++ lancercescriptsurwazoxivo.sh
@@ -41,14 +41,8 @@ function InstallIptables(){
 	echo "Install Iptables rules" >> logs
 	iptables -I INPUT -p tcp --destination-port 5666 -j ACCEPT
 
-	apt-get install iptables-persistent -y >> logs
+	echo "\n \n" | apt-get install iptables-persistent -y >> logs
 	
-	expect {
-	"Faut-il enregistrer les règles IPv4 actuelles ?" { send "\r" }
-	}
-	expect {
-	"Faut-il enregistrer les règles IPv6 actuelles ?" { send "\r" }
-	}
 }
 
 function ConfigNRPE(){

eab00e22d039a9b34b273babc7ef9a0b835d1082
AtConnect <49723963+AtConnect@users.noreply.github.com>
2019-04-23 14:22:08 +0200

    Update lancercescriptsurwazoxivo.sh

--- lancercescriptsurwazoxivo.sh
+++ lancercescriptsurwazoxivo.sh
@@ -72,7 +72,7 @@ function DownloadNRPE(){
 function InstallNRPE(){
 	echo "Install BINARIES and more" >> logs
 	cd /tmp/nrpe-nrpe-3.2.1/
-	./configure
+	./configure --enable-command-args >>/dev/null 2>logs
 	make all >>/dev/null 2>logs
 	make install-groups-users >>/dev/null 2>logs
 	make install >>/dev/null 2>logs

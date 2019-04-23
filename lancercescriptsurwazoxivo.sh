21e6f0d9647fdffd5682c313c8da046f2b815a5e
AtConnect <49723963+AtConnect@users.noreply.github.com>
2019-04-23 16:16:57 +0200

    Update lancercescriptsurwazoxivo.sh

--- lancercescriptsurwazoxivo.sh
+++ lancercescriptsurwazoxivo.sh
@@ -72,10 +72,10 @@ function DownloadNRPE(){
 function InstallNRPE(){
 	cd /tmp/nrpe-nrpe-3.2.1/ || exit
 	./configure --disable-ssl --enable-command-args --enable-install-method=os > /dev/null
-	make all /dev/null
-	make install-groups-users /dev/null
-	make install /dev/null
-	make install-config /dev/null
+	make all > /dev/null
+	make install-groups-users > /dev/null
+	make install > /dev/null
+	make install-config > /dev/null
 	echo >> /etc/services
 	echo '# Nagios services' >> /etc/services
 	echo 'nrpe    5666/tcp' >> /etc/services
